3ffb11d0e4ff38a62b0d7fca29f0891ed9db7e65
eliamfuentes <eliamfuentes@ufcspa.edu.br>
2024-10-16 12:57:08 -0300

    alteração de responsividade

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,99 +20,44 @@ export default class About extends Component {
         <Header />
         <section className="about">
           <div className="about-header-text-skills" >
-            
+
             <div className="about-text-skills">
-            <section className="about-text">
+              <section className="about-text">
                 <p className="about-text-p about-text-p-1">Olá, </p>
-                
+
                 <p className="about-text-p about-text-p-2">meu nome é <span>Eliam Fuentes</span></p>
-                
+
                 <p className="about-text-p about-text-p-3">Sou  estudante de desenvolvimento <span>full-stack</span></p>
-                
+
                 <p className="about-text-p about-text-p-3">As tecnologias que estou utilizando no momento incluem <span>JavaScript</span>, <span>ReactJS</span>, <span>TypeScript</span>, e <span>Sass/CSS</span>, estou constantemente aprimorando minha habilidade em criar interfaces de usuário elegantes e funcionais. Além disso, estou familiarizada com ferramentas essenciais como NodeJS, Bootstrap, SQL e Docker para desenvolvimento de aplicativos robustos</p>
-                
+
                 <p className="about-text-p about-text-p-5">Estou aberto a oportunidades de colaboração, desafios emocionantes e novas conexões na indústria de tecnologia.</p>
 
                 <p className="about-text-p about-text-p-6"><span>Sinta-se à vontade para me chamar para conversar!</span></p>
               </section>
               <div className="about-image">
-            <img className="about-img" src={image} alt="profile" /> 
-          </div>
+                <img className="about-img" src={image} alt="profile" />
+              </div>
               <section className="about-skills">
                 <p className="about-skills-p">SKILLS</p>
                 <div className="about-skill">
-                {hardSkillsIcons.map((icon, index) => (
+                  {hardSkillsIcons.map((icon, index) => (
                     <div key={index} className="about-div-icon">
                       <Icon icon={icon.icon} className={icon.className} />
-                    <p className='about-icon-text'>{icon.title}</p>
+                      <p className='about-icon-text'>{icon.title}</p>
                     </div>
                   ))}
                 </div>
               </section>
             </div>
-            
+
           </div>
-          
+
           <SocialMedia />
         </section>
-        
+
       </div>
-      
+
     )
   }
 }
-
-
-// export default class About extends Component {
-//   render() {
-//     return (
-//       <div className='dark-background'>
-//         <Header />
-
-//         <main className="about">
-        
-//           <section className="about-intro">
-//             <div className="about-text-section">
-//               <p>Olá</p>
-//               <p>meu nome é <span>Danielle Silva</span></p>
-//               <p>Sou desenvolvedora <span>Front-end</span>, mas continuo como estudante da Trybe com o objetivo de me tornar uma desenvolvedora <span>Web Full Stack</span>.</p>
-//               <p>Um pouco sobre minha história, sou <span>bióloga</span> por formação mas resolvi mudar minha vida e dar uma chance para a área da <span>tecnologia</span>, não contava que acabaria me apaixonando, sinto que realmente me encontrei.</p>
-//               <p>Até o momento meu foco é <span>Front-end</span>, mas estou gostando de explorar os horizontes de Back-end também.</p>
-//               <p>Espero que goste do meu portfólio!</p>
-//             </div>
-//             <img className="about-img" src={image} alt="profile" /> 
-//           </section>
-//           <div className="arrow">
-//             <a href='#about' className="arrow-link arrow-point"></a>
-            
-//           </div>
-
-//           <section className="about-skills" id='about'>
-//             <div className="div-skills-title">
-//               <p className="about-skills-title">Habilidade atuais</p>
-//             </div>
-          
-//             <div className="about-both-skills-section about-div-hard-skills-icon-text">
-//               <div className="about-skills-subtitle">
-//                 <p className="about-title">Hard skills</p>
-//               </div>
-              
-//               <div className="about-hard-skills">
-
-//                 {hardSkillsIcons.map((icon) => (
-//                   <div key={icon.id} className="about-div-icon-text">
-//                     <Icon icon={icon.icon} className={icon.className} />
-//                   <p>{icon.title}</p>
-//                   </div>
-//                 ))}       
-//               </div>
-//             </div>
-            
-//           </section>
-          
-//         </main>
-//       </div>
-      
-//     )
-//   }
-// }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 
 import Header from '../components/Header';
-import Footer from '../components/Footer';
 import SocialMedia from '../components/SocialMedia';
+
+import cv from '../assets/Curriculo_Eliam_Fuentes.pdf'
+import { Icon } from '@iconify/react';
 
 import '../styles/pages/home.scss';
 
@@ -16,19 +18,20 @@ export default class Home extends Component {
       <div className='all-home'>
         <Header />
         <div className="home">
-
           <section className="home-section home-text-animation">
             <p className="home-name-1">Eliam</p>
             <p className="home-name-2">Fuentes</p>
             <div className="home-subtitle">
-              <p className="home-subtitle-1">Desenvolvedor front-end</p><span>|</span>
-              <p className="home-subtitle-2">Estudante de Desenvolvimento Web Full Stack</p><span></span>
+              <p className="home-subtitle-1">Desenvolvedor front-end</p>
+            </div>
+            <div className='home-curriculum'>
+              <button className='home-curriculum-button'>
+                <a href={cv} download='Curriculo_Eliam_Fuentes'>Baixar CV <Icon icon="la:download" /></a>
+              </button>
             </div>
           </section>
           <SocialMedia />
         </div>
-
-        <Footer />
       </div>
 
     )
